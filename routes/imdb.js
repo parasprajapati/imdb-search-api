77079eabfddde20b9736803e97842e863c36c2a5
Paras Prajapati <paras@mrparas.com>
2018-12-13 19:05:05 -0500

    updated endpoint

--- routes/imdb.js
+++ routes/imdb.js
@@ -3,20 +3,25 @@ var request = require('request');
 var cheerio = require('cheerio');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-
+/* GET titles search result. */
+router.get('/title', function(req, res, next) {
+  var results = [];
   url = 'https://www.imdb.com/find?q=avatar&s=tt&ref_=fn_al_tt_mr';
   request(url, function(error, response, html) {
     if (!error && response.statusCode == 200) {
       $ = cheerio.load(html);
       let result_list = $('.findList').children().first().children();
       result_list.each(function(i, elem) {
-        console.log(elem.children().first().text());
+        results[i] = {
+          name: $(this).children().last().text(),
+          link: 'https://www.imdb.com' + $(this).children().last().children().first().attr('href'),
+          image: $(this).children().first().children().first().children().first().attr('src'),
+        };
       });
+      res.send(results);
     }
   })
-  res.send('Check your console!');
+  
 
 });
 
