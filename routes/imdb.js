0916665092b95152156fcf4be9b436e715c93cf5
Paras Prajapati <paras@mrparas.com>
2018-12-13 19:50:35 -0500

    added second endpoint

--- routes/imdb.js
+++ routes/imdb.js
@@ -4,9 +4,9 @@ var cheerio = require('cheerio');
 var router = express.Router();
 
 /* GET titles search result. */
-router.get('/title', function(req, res, next) {
+router.get('/search/title', function(req, res, next) {
   var results = [];
-  url = 'https://www.imdb.com/find?q=avatar&s=tt&ref_=fn_al_tt_mr';
+  url = 'https://www.imdb.com/find?q=' + req.param('search_query') + '&s=tt&ref_=fn_al_tt_mr';
   request(url, function(error, response, html) {
     if (!error && response.statusCode == 200) {
       $ = cheerio.load(html);
@@ -14,15 +14,31 @@ router.get('/title', function(req, res, next) {
       result_list.each(function(i, elem) {
         results[i] = {
           name: $(this).children().last().text(),
-          link: 'https://www.imdb.com' + $(this).children().last().children().first().attr('href'),
+          title_id: $(this).children().last().children().first().attr('href'),
           image: $(this).children().first().children().first().children().first().attr('src'),
         };
       });
       res.send(results);
     }
   })
-  
+});
 
+/* GET info about a specific title. */
+router.get('/get/title', function(req, res, next) {
+  var results = {
+    title: '',
+    rating: ''
+  };
+  url = 'https://www.imdb.com' + req.param('title_id');
+  request(url, function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+      $ = cheerio.load(html);
+      let basic_info = $('.title_wrapper').children();
+      results.title = basic_info.first().text();
+      results.rating = basic_info.last().text();
+      res.send(results);
+    }
+  })
 });
 
 module.exports = router;
